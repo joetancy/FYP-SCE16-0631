07fceb00d8476fc37f32918fc6ab38869959db2e
Joe Tan <joetanchinyong@gmail.com>
2017-08-24 16:42:22 +0800

    added gaussian noise and jpeg compression

--- imageDistortion.py
+++ imageDistortion.py
@@ -12,9 +12,10 @@ imageDir = input('Image directory: ')
 saveImageDir = input('Save image directory: ')
 
 distortionType = input(
-    '1. Gaussian Blur\n2. Gaussian Noise\n3. JPEG Compression\nEnter you choice: ')
+    '1. Gaussian Blur\n2. Gaussian Noise\n3. JPEG Compression\nEnter your choice: ')
 
-if distortionType == 1:
+if distortionType == '1':
+    print('Gaussian Blur\n')
     # gaussian blur
     i = 0
     for root, dirs, files in os.walk("./" + imageDir):
@@ -26,11 +27,12 @@ if distortionType == 1:
                     image = skimage.util.img_as_float(image, force_copy=False)
                     image = filters.gaussian(image, sigma=3, truncate=2)
                     time.sleep(0.1)
-                    file = './' + saveImageDir + 'gaussian/' + file
+                    file = './' + saveImageDir + '/' + file
                     skimage.io.imsave(file, image)
                 i += 1
                 bar.update(i)
-elif distortionType == 2:
+elif distortionType == '2':
+    print('Gaussian Noise\n')
     # gaussian noise
     i = 0
     for root, dirs, files in os.walk("./" + imageDir):
@@ -43,11 +45,12 @@ elif distortionType == 2:
                     image = utils.random_noise(
                         image, mode='gaussian', clip=True)
                     time.sleep(0.1)
-                    file = './' + saveImageDir + 'gaussian/' + file
+                    file = './' + saveImageDir + '/' + file
                     skimage.io.imsave(file, image)
                 i += 1
                 bar.update(i)
-elif distortionType == 3:
+elif distortionType == '3':
+    print('JPEG Compression\n')
     # jpeg compression
     i = 0
     for root, dirs, files in os.walk("./" + imageDir):
@@ -58,9 +61,7 @@ elif distortionType == 3:
                     image = skimage.io.imread(os.path.join(root, file))
                     image = skimage.util.img_as_float(image, force_copy=False)
                     time.sleep(0.1)
-                    file = './' + saveImageDir + 'gaussian/' + file
-                    skimage.io.imsave(file, image)
+                    file = './' + saveImageDir + '/' + file
+                    skimage.io.imsave(file, image, quality=25)
                 i += 1
                 bar.update(i)
-else:
-    break
